4a2f7285230ee115baa708760040c9e15a47be54
tbritton03 <Ziberath@gmail.com>
2016-05-30 16:52:51 -0500

    Following the HTTP tutorial on the angular js website.

--- app/game.data.service.ts
+++ app/game.data.service.ts
@@ -1,12 +1,34 @@
 import { Injectable }    from '@angular/core';
-import { Headers, Http } from '@angular/http';
+import { Headers, Http, Response } from '@angular/http';
+import { Observable }     from 'rxjs/Observable';
+import {Game} from './game.model'
 
 @Injectable()
 export /**
  * BackEndService
  */
-class BackEndService {
-    constructor(private http: Http) {
-        
+    class BackEndService {
+    constructor(private http: Http) { }
+    GAME_FIND_API_URL = "";
+
+
+    getGames(): Observable<Game[]> {
+        return this.http.get(this.GAME_FIND_API_URL)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    private extractData(res: Response) {
+        let body = res.json();
+        return body.data || {};
+    }
+
+    private handleError(error: any) {
+        // In a real world app, we might use a remote logging infrastructure
+        // We'd also dig deeper into the error to get a better message
+        let errMsg = (error.message) ? error.message :
+            error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+        console.error(errMsg); // log to console instead
+        return Observable.throw(errMsg);
     }
 }
