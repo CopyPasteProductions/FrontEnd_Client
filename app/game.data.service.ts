9fed4748589dbefd37f385473d70697076cd87f0
ThePolitician170 <emilianoponce@Emilianos-Mac-mini.local>
2016-06-23 20:24:13 -0500

    Revert "Emiliano made changes"
    This reverts commit 754bfb773c456a25cb04c6e6797379d5de4c4bf1.

--- app/game.data.service.ts
+++ app/game.data.service.ts
@@ -6,7 +6,7 @@ import {Game} from './game.model'
 // Statics
 import 'rxjs/add/observable/throw';
 
-// Operators EGP rules
+// Operators
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
